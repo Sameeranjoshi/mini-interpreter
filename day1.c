3616e95c0b02d35e71179b35fc380396b14cb3e2
Sameeranjoshi <joshisameeran17@gmail.com>
2018-06-27 18:25:46 +0530

    started accepting whitespaces

--- day1.c
+++ day1.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
 #include<ctype.h>
+#include<string.h>
+
 #define NONE_OF_THESE 0
-#define OPERAND 1
-#define OPERATOR 2
+#define DIGIT 1
+#define PLUS 3
+#define MINUS 4
+#define MULTIPLY 5
+#define DIVIDE 6
 struct info_about_tokens{
 	int d_type;
 	char value;
@@ -10,16 +15,30 @@ struct info_about_tokens{
 void gettoken(char *position,struct info_about_tokens *ptr_to_str){
 	
 	if(isdigit(*position)){
-				ptr_to_str->d_type=OPERAND;
+				ptr_to_str->d_type=DIGIT;
 				ptr_to_str->value=(*position);
 	}
 	else if(((*position)=='+')||((*position)=='-')||((*position)=='*')||((*position)=='/')||((*position)=='=')){
-				ptr_to_str->d_type=OPERATOR;
+				switch(*position){
+					case '+':
+						ptr_to_str->d_type=PLUS;
+						break;	
+					case '-':
+						ptr_to_str->d_type=MINUS;
+						break;
+					case '*':
+						ptr_to_str->d_type=MULTIPLY;
+						break;
+					case '/':
+						ptr_to_str->d_type=DIVIDE;
+						break;						
+				}
+
 				ptr_to_str->value=(*position);	
 	}
 	else {
 				ptr_to_str->d_type=NONE_OF_THESE;
-				ptr_to_str->value='N';		//INSERT A NULL IN VALUE
+				ptr_to_str->value='N';		//INSERT AS NULL VALUE
 	}
 
 }
@@ -28,18 +47,27 @@ int main(void){
 	char input_expression[30];
 	struct info_about_tokens exp_info[10];
 	printf("\nENTER THE EXPRESSION");
-	scanf("\n%s",input_expression);
+	scanf("%[^\n]s",input_expression);	//accepts spaces now
 	printf("\nTHE EXPRESSION IS: %s",input_expression);
 	
 	char *position;	
 	position=input_expression;
 	int token_cnt=0;
-	int i=0;
+	int i=0;		
+	
+
 	while((*position)!='\0'){
-		gettoken(position,&exp_info[i]);
-		token_cnt++;
-		i++;
-	position++;		
+		if(isspace(*position)){
+			position++;
+			printf("\nskipped a space");
+		}
+		else{
+			gettoken(position,&exp_info[i]);
+			token_cnt++;
+			i++;
+			position++;		
+		}		
+
 	}
 
 	printf("\nTHE EXPRESSION STRUCTURE IS(after lexing):");
