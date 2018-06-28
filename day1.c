0ff0ff681de3bfa1f39aaeacc73e08c53ce513db
Sameeranjoshi <joshisameeran17@gmail.com>
2018-06-28 20:36:13 +0530

    only syntax is checked correctly for *+-/

--- day1.c
+++ day1.c
@@ -48,48 +48,126 @@ int validate(int data_type,int identifier){
 	else
 		return 0;
 }
-int parse(struct info_about_tokens *ptr,int token_cnt){
-	int i=0;
+int operator(int data_type){
+	//operator:PLUS|MINUS|MULTIPLY|DIVIDE
+	if((data_type==PLUS)||(data_type==MINUS)||(data_type==MULTIPLY)||(data_type==DIVIDE))
+		return 1;
+	else
+		return 0;
+}
+int factor(int data_type){
+	//factor:INTEGER|ALPHABET|VARIABLE
+	printf("\tfactor called");
+	if(data_type==DIGIT)
+		return 1;
+	else 
+		return 0;
+}
+int term(struct info_about_tokens *ptr,int token_cnt,int globalindex){
+	printf("\tterm called");
+	/*
+		term:factor (((mul)|(div))factor)*
+	*/	
+
 	int isvalid;
 	
-	isvalid=validate(ptr->d_type,DIGIT);
+	isvalid=factor(ptr->d_type);
 	if(isvalid){
+		
 		token_cnt--;
 		ptr++;
-		/*this checks for the initial condition (ex.INPUT = 2)
+		globalindex++;			//goes to next ele
+		/*this checks for the initial condition (ex.INPUT = 2)(this saves you!!)
 		*/
-		if(token_cnt==0)
-			return 0;
+//		if(token_cnt==0)
+//			return 0;
 		
 		while(token_cnt!=0){
-	//if operator matches
-	if((validate(ptr->d_type,PLUS)||validate(ptr->d_type,MINUS)||validate(ptr->d_type,MULTIPLY)||validate(ptr->d_type,DIVIDE)))
+			//if operator matches
+			
+			if((ptr->d_type==MULTIPLY)||(ptr->d_type==DIVIDE))
 			{
-
+				printf("\tOPERATOR MATCH FOUND");
 				ptr++;
 				token_cnt--;
-				if(validate(ptr->d_type,DIGIT)){//now the 2nd para matches
+				if(factor(ptr->d_type)){//now the 2nd para matches
 					ptr++;
 					token_cnt--;
+
+			//why 2 times?as we check DIGIT OPERATOR then only increase the count else invalid
+					globalindex++;
+					globalindex++;
 				}
 				else
-					return 0;	
+					return (-1);	//-1 for no match
+			}
+			else if((ptr->d_type==PLUS)||(ptr->d_type==MINUS)){
+				return globalindex;//ex like = 2+
 			}
 			else{
-				return 0;
+				return (-1);		//ex. like = 2 3
 			}
-
 		
 		}
-		return 1;
+	return globalindex;
 	
 	}
 	else{
-		return 0;
+		return (-1);
 	}
 
 
 }
+int expr(struct info_about_tokens *ptr,int token_cnt){//remaining to complete
+	printf("\texpr called");
+
+	struct info_about_tokens *start_address=ptr;
+	int isvalid,globalindex=0;
+	globalindex=term(ptr,token_cnt,globalindex);
+	printf("\nINSIDE EXPR GLOBALINDEX IS :%d\nprinting array",globalindex);
+	for(int i=0;i<token_cnt;i++)
+		printf("\n%d",ptr[globalindex].value);
+
+//	printf("\nprt is %d index is %d",ptr,globalindex);
+	ptr=start_address+globalindex;		//take ptr ahead
+
+//	printf("\nprt is %d index is %d",ptr,globalindex);
+
+	printf("\n%d %c ",ptr->d_type,ptr->value);
+	
+	int save;
+	if(globalindex!=-1){
+//				printf("\ndatatype%c %d globalindex %d",ptr[globalindex-1].value,ptr->d_type,globalindex);
+		while(globalindex<token_cnt){
+			if((ptr->d_type==PLUS)||(ptr->d_type==MINUS)){
+				globalindex++;
+				printf("\nsameeran %d",globalindex);
+			    if(globalindex<token_cnt){
+					if(factor(start_address[globalindex].d_type)){
+						printf("trapped");
+						globalindex++;
+					}
+					else{	
+						printf("\nin else %d",globalindex);
+						return 0;	
+					}
+			    }		
+			    else{
+				return 0;//if like e.x=2+ and string ends after +
+			    }			
+			}
+			else{
+				return 1;//the term(...)* the expression in () isnottrue	
+			}
+		}
+		return 1;//this says the term has already checked all(ex. 2*3*5)
+	}
+	else{
+		return 0;//term says the expression is wrong
+	}
+}
+
+
 int main(void){
 	
 	char input_expression[30];
@@ -119,12 +197,12 @@ int main(void){
 	}
 
 	printf("\nTHE EXPRESSION STRUCTURE IS(after lexing):");
-	printf("\n(DATATYPE,VALUE)");
+	printf("\n(DATATYPE,VALUE)\n1=DIGIT\n3=PLUS\n4=MINUS\n5=MULTIPLY\n6=DIVIDE");
 	for(int i=0;i<token_cnt;i++){
 		printf("\n (%d,%c)",exp_info[i].d_type,exp_info[i].value);
 	}
 	
-	int success=parse(exp_info,token_cnt);	
+	int success=expr(exp_info,token_cnt);
 	if(success)
 		printf("\nFORMAT MATCHES");
 	else
