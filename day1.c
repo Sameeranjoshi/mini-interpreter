b67238ca0c5396360b5d39d3b0e275dfbde7cd17
Sameeranjoshi <joshisameeran17@gmail.com>
2018-06-30 02:42:11 +0530

    evaluation done.just toggled a bit,remove it later

--- day1.c
+++ day1.c
@@ -9,14 +9,15 @@
 #define MINUS 4
 #define MULTIPLY 5
 #define DIVIDE 6
-
+#define LPAREN 7
+#define RPAREN 8
 
 struct info_about_tokens{
 	int d_type;
 	char value;		//beaware it's in char when value needed for evaluation parse it!
 };
 
-struct info_about_tokens stack[10];
+struct info_about_tokens stack[30];
 int top=-1;
 
 void push(struct info_about_tokens *ptr){
@@ -39,7 +40,7 @@ struct node{
 	struct node* left;
 	struct node* right;	
 };
-struct node *pointer_stack[10];
+struct node *pointer_stack[30];
 int AST_top=-1;
 
 void push_AST(struct node *holding_ptr){
@@ -52,14 +53,14 @@ struct node* pop_AST(){
 		AST_top--;
 	return p;
 }
-struct info_about_tokens new_postfixed[20];//conntains the postfixed array
+struct info_about_tokens new_postfixed[30];//conntains the postfixed array
 void gettoken(char *position,struct info_about_tokens *ptr_to_str){
 	
 	if(isdigit(*position)){
 				ptr_to_str->d_type=DIGIT;
 				ptr_to_str->value=(*position);
 	}
-	else if(((*position)=='+')||((*position)=='-')||((*position)=='*')||((*position)=='/')||((*position)=='=')){
+	else if(((*position)=='+')||((*position)=='-')||((*position)=='*')||((*position)=='/')||((*position)=='=')||((*position)==')')||((*position)=='(')){
 				switch(*position){
 					case '+':
 						ptr_to_str->d_type=PLUS;
@@ -72,7 +73,13 @@ void gettoken(char *position,struct info_about_tokens *ptr_to_str){
 						break;
 					case '/':
 						ptr_to_str->d_type=DIVIDE;
-						break;						
+						break;
+					case '(':
+						ptr_to_str->d_type=LPAREN;
+						break;
+					case ')':
+						ptr_to_str->d_type=RPAREN;
+						break;
 				}
 
 				ptr_to_str->value=(*position);	
@@ -195,8 +202,8 @@ void print_info_struct(struct info_about_tokens *loopptr,int token_cnt){
 		loopptr++;
 	}
 }
-void print_postfix(struct info_about_tokens *loopptr,int token_cnt){
-	for(int i=0;i<token_cnt;i++){
+void print_postfix(struct info_about_tokens *loopptr,int cnt_except_paren){
+	for(int i=0;i<cnt_except_paren;i++){
 		printf("%c",loopptr->value);
 		loopptr++;
 	}
@@ -214,6 +221,14 @@ int isoperator(int data_type){
 	else
 		return 0;
 }
+int isparen(int data_type){
+
+	if((data_type==LPAREN)||(data_type==RPAREN))
+		return 1;
+	else
+		return 0;
+}
+
 int check_precedence(char operator_val){
 	int prec_on_list,prec_on_stack;
 	if(operator_val=='+'||operator_val=='-')
@@ -230,6 +245,9 @@ int check_precedence(char operator_val){
 		case '*':
 		case '/':
 			prec_on_stack=2;
+			break;
+		case '(':
+			prec_on_stack=0;//has to have always smaller precedence than others
 			break;
 		}
 	}
@@ -274,6 +292,25 @@ struct info_about_tokens* postfix(struct info_about_tokens *postfix_ptr,int toke
 
 			}
 		}
+		else if(isparen(postfix_ptr->d_type)){
+			
+			switch(postfix_ptr->value){
+				case '(':					
+					push(postfix_ptr);
+					break;
+					
+				case ')':
+										
+					while((stack[top].value!='(')){
+						struct info_about_tokens *hold=pop();
+						new_postfixed[new_index].d_type=hold->d_type;
+						new_postfixed[new_index].value=hold->value;
+						new_index++;
+					}
+					struct info_about_tokens *del=pop();
+					break;
+			}
+		}
 		postfix_ptr++;
 	}
 	while(top>=0){//why >= as need also the value of 0 location of stack
@@ -287,16 +324,19 @@ struct info_about_tokens* postfix(struct info_about_tokens *postfix_ptr,int toke
 	return temp_ptr;
 }
 
-struct node* AST(struct info_about_tokens *ptr_struct,int token_cnt){
+struct node* AST(struct info_about_tokens *ptr_struct,int token_cnt,int cnt_except_paren){
+printf("\vnumber %d",cnt_except_paren);
+
 
 	ptr_struct=postfix(ptr_struct,token_cnt);
 	printf("\nPOSTFIX EXPRESSION IS:");
-	print_postfix(ptr_struct,token_cnt);
+	print_postfix(ptr_struct,cnt_except_paren);
 
 	struct info_about_tokens *loopptr=ptr_struct;
 	
 	
-	for(int i=0;i<token_cnt;i++){
+	for(int i=0;i<cnt_except_paren;i++){
+
 		if(isoperator(loopptr->d_type)){	
 			//CREATE A STRUCTURE ,FILL THE VALUES
 			struct node *ptr_to_node;
@@ -304,11 +344,11 @@ struct node* AST(struct info_about_tokens *ptr_struct,int token_cnt){
 			ptr_to_node->d_type=loopptr->d_type;
 			ptr_to_node->value=loopptr->value;//note=the values are in character,while eval. convert to integer
 	
-			//POP FROM AST ASSIGN TO LEFT AND RIGHT
+			//POP FROM AST ASSIGN TO RIGHT AND THEN LEFT
 			ptr_to_node->right=pop_AST();
 			ptr_to_node->left=pop_AST();
 
-			push_AST(ptr_to_node);//pass the pointer POINTING TO '+' 
+			push_AST(ptr_to_node);//pass the pointer POINTING TO 'operator'
 			
 		}
 		else{			
@@ -323,40 +363,75 @@ struct node* AST(struct info_about_tokens *ptr_struct,int token_cnt){
 		}
 	loopptr++;
 	}
-	
+//	printf("\nTOP STACK %c",pointer_stack[AST_top]->value);
 	
 	
 	struct node *ROOT;
 	ROOT=pop_AST();
+//	printf("\nINGLE%c",ROOT->value);
 	return ROOT;
 }
-void inorder(struct node *alias_root){
+
+/*
+	I sware :D I copied code from geeksforgeeks,but was fun printing and seeing tree build for first time.
+*/
+void inorder(struct node *alias_root,int space){
+	//printf("\n%c",alias_root->value);
 	if(alias_root==NULL)
 		return ;
-	
-	inorder(alias_root->left);
+	space+=5;
+	inorder(alias_root->right,space);
 
-	printf("  %c",alias_root->value);
+	printf("\n");
+	for(int i=5;i<space;i++)
+		printf(" ");
+	printf("%c\n",alias_root->value);
 	
-	inorder(alias_root->right);
+	inorder(alias_root->left,space);
 
+}
+int eval(struct node *alias_root){
+	if(!alias_root)
+		return 0;
+	else if((!alias_root->left)&&(!alias_root->right)){
+		printf("\n\t%d",alias_root->value-'0');
+		return alias_root->value-'0';
+	}
+	int l_value=eval(alias_root->left);
+	int r_value=eval(alias_root->right);
+	
+	if(alias_root->value=='+')
+		return l_value+r_value;
+	if(alias_root->value=='-')
+		return l_value-r_value;
+	if(alias_root->value=='*')
+		return l_value*r_value;
+	if(alias_root->value=='/')
+		return l_value/r_value;	
 }
 int main(void){
 	
 	char input_expression[30];
-	struct info_about_tokens exp_info[10];
+	struct info_about_tokens exp_info[30];
 	struct node *root;//pointer to the printing structure
 	printf("\nENTER THE EXPRESSION");
 	scanf("%[^\n]s",input_expression);	//accepts spaces now
-	printf("\nTHE EXPRESSION IS: %s",input_expression);
+
+	printf("\nTHE EXPRESSION IS: %s",input_expression);		
+
 	
-	char *position;	
+	char *position;
 	position=input_expression;
 	int token_cnt=0;
-	int i=0;		
+	int i=0,cnt_except_paren=0;		
+	
 	
 
+int no_of_paren=0;
 	while((*position)!='\0'){
+//		printf("\nvalue %d %d",*position,'(');
+		if((*position=='(')||(*position==')'))
+			no_of_paren++;
 		if(isspace(*position)){
 			position++;
 		}
@@ -364,26 +439,30 @@ int main(void){
 			gettoken(position,&exp_info[i]);
 			token_cnt++;
 			i++;
-			position++;		
-		}		
-
+			position++;
+		}
 	}
 
-	printf("\n\v\t\t######	TOKENIZATION DONE	######");
-	printf("\nLIST OF TOKENS[0=NONE|1=DIGIT|3=PLUS|4=MINUS|5=MULTIPLY|6=DIVIDE]");
+	printf("\n\v\t\t######	TOKENIZATION DONE	###### ");
+	printf("\nLIST OF TOKENS[0=NONE|1=DIGIT|3=PLUS|4=MINUS|5=MULTIPLY|6=DIVIDE|7=LPAREN|8=RPAREN]");
 	for(int i=0;i<token_cnt;i++){
 		printf("\n (%d,%c)",exp_info[i].d_type,exp_info[i].value);
 	}
 	
+	cnt_except_paren=token_cnt-no_of_paren;
+//	printf("\nexcept paren %d",cnt_except_paren);
 	int success=expr(exp_info,token_cnt);
 	printf("\n\v\t\t######	PARSING DONE	######");
-	if(success){
+	if(!success){
 		printf("\nFORMAT MATCHES");
 		printf("\n\v\t\t######	CREATING AST	######");
-		root=AST(exp_info,token_cnt);
-		printf("\nAST CREATED\nprinting the indorder of AST");
-		inorder(root);
-				
+		root=AST(exp_info,token_cnt,cnt_except_paren);
+		printf("\nAST CREATED\nprinting the indorder of AST\n");
+		printf("-------------------------------------------");
+		inorder(root,0);
+		printf("-------------------------------------------");
+		int ans=eval(root);
+		printf("\v\v %d",ans);
 	}
 	else
 		printf("\nFORMAT DOSEN'T MATCHES");
